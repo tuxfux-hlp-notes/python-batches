64d4fb5fc3c5616747693e9f44592d357979cabb
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2016-12-20 20:09:20 +0530

    dec 20th

---
+++ batch-57/details.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+name = raw_input("please enter your name:")
+gender = raw_input("please enter your gender:")
+print "your name is {} and your gender is {}".format(name,gender)
---
+++ batch-57/market.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# pass : block holder
+
+print "welcome to the market"
+food_type = raw_input("what is your food type - fish/meat/chicken/veg :")
+
+if food_type == 'fish':
+    print "welcome to the fish market"
+    fish_type = raw_input("what fish you need - cod/tuna/solomon/rohu:")
+    if fish_type == 'cod':
+        print "we have you fish type {} available".format(fish_type)
+        print "how much quantity of fish type {} you need".format(fish_type)
+    elif fish_type == 'tuna':
+        print "we have you fish type {} available".format(fish_type)
+        print "how much quantity of fish type {} you need".format(fish_type)
+    elif fish_type == 'rohu':
+        print "we have you fish type {} available".format(fish_type)
+        print "how much quantity of fish type {} you need".format(fish_type)
+    else:
+        print "we don't have your fish type {} available".format(fish_type)
+        print "how about chicken/meat/veg"
+elif food_type == 'meat':
+    pass
+elif food_type == 'chicken':
+    pass
+elif food_type == 'veg':
+    pass
+else:
+    pass
+    
+    
