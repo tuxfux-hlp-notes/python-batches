1c4e1c4d34139ca8b15916f1627a5a78bf04ac86
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-02-09 10:12:49 +0530

    latest batch 58

---
+++ batch-58/first.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# Author : Santosh
+# date: Feb 9th
+# Usage: saying hello to the world
+# Log: 10-02-2017  - kumar - i added one more exclamation to the code.
+print "hello world !!!"
