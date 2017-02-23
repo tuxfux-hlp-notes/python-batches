2995cfdcec96e166650fc1e5737568dd39d4f53c
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-02-23 09:07:59 +0530

    modules and functions

---
+++ batch-58/modules/first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+version = 2.0
+
+def my_add(a,b):
+	''' This is for addition of number/strings '''
+	return a + b
+
+def my_sub(a,b):
+	''' This is for substraction of number '''
+	return a - b
+
+def my_div(a,b=1):
+	''' This is for division of numbers '''
+	return a/b
+
+def my_multi(a,b):
+	''' This is for multiplication of numbers '''
+	return a * b
