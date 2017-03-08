50a7700c3d360d5444f199e88b689a1a32f3d59b
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-03-08 10:05:51 +0530

    debugging notes

---
+++ batch-58/debugging/buggy.py
@@ -0,0 +1,6 @@
+def divide_one_by(division):
+  return 1/division
+
+ # Main
+if __name__ == '__main__':
+  divide_one_by(0)
---
+++ batch-58/debugging/third.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import pdb
+
+def first():
+  second()
+  return "hey i am first"
+
+def second():
+  third()
+  return "hey i am second"
+
+def third():
+  fourth()
+  return "hey i am third"
+
+def fourth():
+  fifth()
+  return "hey i am fourth"
+
+def fifth():
+  return "hey i am fifth"
+
+pdb.set_trace()
+first()
