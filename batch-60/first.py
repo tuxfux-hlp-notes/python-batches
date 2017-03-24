5f4e60c3835fa1e66a64ec82ede24c4379ff1c82
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-03-24 19:23:28 +0530

    Installation class

---
+++ batch-60/first.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# Author: kumar
+# usage: saying hello to the world.
+# Log: March 24th - kumar - i wrote a hello world program.
+#    : march 25th - san - i added one more !
+print ("hello world !!!!")
