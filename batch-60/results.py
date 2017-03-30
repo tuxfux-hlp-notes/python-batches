77ae85719584b0015edd16c82c74ee21455f6b52
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-03-30 19:22:59 +0530

    Learning conditions and loops

---
+++ batch-60/results.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# continue: skip an iteration.
+
+for student in ['sravanti','thakur','kumar','ramya','khiri','rahul']:
+    if student == 'kumar':
+        continue
+        #break
+        #pass
+    print "results of - {}".format(student)
