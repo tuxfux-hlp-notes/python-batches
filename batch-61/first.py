660e7e08091d87286aa171915ff00ccf13e207f4
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-04-07 09:14:09 +0530

    help and firstprogram

---
+++ batch-61/first.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# Author:kumar
+# Usage: I want to say hello to everyone.
+# logs: Apr7-17 - kumar - initial code.
+#     : Apr7-18 - jeevan - removed an !
+print ("hello world!!")
+
+
+
+
+'''
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ls
+first.py  My first code.ipynb  opensource.txt  Python_Installation.txt
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ python first.py
+hello world!!!
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ perl first.py
+hello world!!!tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ 
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ 
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ls -l
+total 16
+-rw-r--r-- 1 tcloudost tcloudost   23 Apr  7 08:15 first.py
+-rw-r--r-- 1 tcloudost tcloudost  864 Apr  6 08:44 My first code.ipynb
+-rw-r--r-- 1 tcloudost tcloudost 1737 Apr  5 08:52 opensource.txt
+-rw-r--r-- 1 tcloudost tcloudost 2145 Apr  6 18:12 Python_Installation.txt
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ls -l
+total 16
+-rwxr-xr-x 1 tcloudost tcloudost   23 Apr  7 08:15 first.py
+-rw-r--r-- 1 tcloudost tcloudost  864 Apr  6 08:44 My first code.ipynb
+-rw-r--r-- 1 tcloudost tcloudost 1737 Apr  5 08:52 opensource.txt
+-rw-r--r-- 1 tcloudost tcloudost 2145 Apr  6 18:12 Python_Installation.txt
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ 
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ./first.py
+Warning: unknown mime-type for "hello world!!!" -- using "application/octet-stream"
+Error: no such file "hello world!!!"
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ which python
+/usr/bin/python
+
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ python -V
+Python 2.7.6
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ls
+first.py  My first code.ipynb  opensource.txt  Python_Installation.txt
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ which python
+/usr/bin/python
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ /usr/bin/python -V
+Python 2.7.6
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ./first.py
+hello world!!!
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ which python3
+/usr/bin/python3
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ./first.py
+  File "./first.py", line 3
+    print "hello world!!!"
+                         ^
+SyntaxError: Missing parentheses in call to 'print'
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ./first.py
+hello world!!!
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ ./first.py
+hello world!!!
+tcloudost@tcloudost-VirtualBox ~/Documents/git_repositories/python-batches/batch-61 $ 
+
+
+
+
+'''
