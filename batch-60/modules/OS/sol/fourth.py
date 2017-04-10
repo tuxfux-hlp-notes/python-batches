94031d92c37770c84196f142be7a7aabb912407e
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-04-10 19:22:15 +0530

    packages

---
+++ batch-60/modules/OS/sol/fourth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def my_first_sol4():
+	return "this is my first sol4 function"
+
+def my_second_sol4():
+	return "this is my second sol4 function"
+
+def my_third_sol4():
+	return "this is my third sol4 function"
+
+def my_fourth_sol4():
+	return "this is my fourth sol4 function"
