59a7316f78863859bbefd577a25802822177582d
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-04-26 09:28:00 +0530

    modules - day1

---
+++ batch-61/modules/first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+version = 2.0
+
+def my_add(a,b):
+	'''
+		This is for addition of two numbers or strings. 
+		syntax: my_add(11,22)
+	'''
+	return a + b
+
+def my_sub(a,b):
+	'''
+		This is for substraction of two numbers.
+	'''
+	if a > b:
+		return a - b
+	else:
+		return b - a
+
+def my_div(a,b):
+	'''
+		This is for division of two numbers.
+	'''
+	return a/b
+
+def my_multi(a,b):
+	'''
+		This is for multiplication of two numbers.
+	'''
+	return a * b
+
+# Main
+print "Launching a missile"
