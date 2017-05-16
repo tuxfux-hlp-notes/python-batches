555cd4662fb80e43a7d2548fe57b5b3e09b622f2
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-05-16 19:35:03 +0530

    day 2 notes

--- batch-62/first.py
+++ batch-62/first.py
@@ -1,2 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# Author: kumar
+# usage : saying hello to the world.
+# logs  : 16-05-2017 - kumar -first program
+#       : 18-05-2019 - new modification
 print ("hello world !!!")
