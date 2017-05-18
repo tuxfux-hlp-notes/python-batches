c802fe1a74eb9cb45966abb383c2a23e48c21526
tuxfux.hlp.cbt <tuxfux.hlp.cbt@gmail.com>
2017-05-18 19:06:26 +0530

    looping

---
+++ batch-62/results.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# continue: skips an iteration.
+# 
+for student in ['shravya','anu','deepti','kumar','naimisha','varun','mani']:
+    if student == 'kumar':
+        continue
+        #break
+        #pass
+    print "results of student - {}".format(student)
