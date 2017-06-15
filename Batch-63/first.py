82eebe166b35baf16a93786a25dd2108a053dbb2
tuxfux-hlp-notes <tuxfux-hlp-notes@gmail.com>
2017-06-15 19:03:28 +0530

    my first program

---
+++ Batch-63/first.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#hash -> ash !-> exclamation -> bang
+# Author: kumar
+# Usage: This is a program to say hello to the world.
+# creation date: 15th June
+# Logs: 16th june : i added a smile to the world.
+print ("hello world!!! :)")
