d64df9e8d116191d94538a3dc33c7564788cd0a5
tuxfux-hlp-notes <tuxfux-hlp-notes@gmail.com>
2017-06-22 17:05:27 +0530

    day befores notes

---
+++ Batch-63/second.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+name = raw_input("please enter your name:")
+age = raw_input("please enter your age:")
+print "your name is {} and age is {}".format(name,age)
+
+
+'''
+Khyaathi Python: pwd
+/home/khyaathipython
+Khyaathi Python: ls
+Desktop    Downloads         Music     Public     Untitled0.ipynb
+Documents  examples.desktop  Pictures  Templates  Videos
+Khyaathi Python: 
+Khyaathi Python: ls
+Desktop    Downloads         Music     Public     Untitled0.ipynb
+Documents  examples.desktop  Pictures  Templates  Videos
+Khyaathi Python: 
+Khyaathi Python: cd Documents/
+Khyaathi Python: ls
+git_repos
+Khyaathi Python: cd git_repos/
+Khyaathi Python: ls
+python-batches
+Khyaathi Python: cd python-batches/
+Khyaathi Python: ls
+archieves  Batch-63  LICENSE  README.md  test
+Khyaathi Python: cd Batch-63/
+Khyaathi Python: ls
+batch-63.txt     Helping_ipython_notebook.ipynb  opensource.txt
+first.py         Installation_python.txt         second.py
+helloguys.ipynb  Learning_help.txt
+Khyaathi Python: python second.py 
+please enter your name:kumar
+please enter your age:56
+your name is kumar and age is 56
+Khyaathi Python: 
+
+
+
+
+'''
