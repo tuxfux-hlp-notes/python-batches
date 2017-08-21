85a2375902aaa86d3b9c084ceddcac4203e64455
tuxfux-hlp-notes <tuxfux-hlp-notes@gmail.com>
2017-08-21 19:38:23 +0530

    exception handling

---
+++ batch-64/10-exceptions/first.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# example 1:
+num1 = int(raw_input("please enter the number1:"))
+num2 = int(raw_input("please enter the number2:"))
+result = num1/num2
+print "the result is {}".format(result)
