d83471ca0ee2e0892f2886799d77840b87f592f3
tuxfux-hlp-notes <tuxfux.hlp@gmail.com>
2017-11-02 19:36:15 +0530

    removed old files

--- batch-66/Lists_results.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# continue - skip your iteration
-absent = ['kumar','ram','navya']
-for student in ('mayuri','sowji','priyanka','kumar','ravi','reddy','anand','ram','navya'):
-	if student in absent:
-		continue
-		#break
-		#pass
-	print "results of student - {}".format(student)
--- batch-66/results.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# continue - skip your iteration
-for student in ('mayuri','sowji','priyanka','kumar','ravi','reddy','anand'):
-	if student == 'kumar':
-		continue
-		#break
-		#pass
-	print "results of student - {}".format(student)
