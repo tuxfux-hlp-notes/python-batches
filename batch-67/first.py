598c32da43e0dc5848938e00f562378427637ddb
tuxfux-hlp-notes <tuxfux.hlp@gmail.com>
2017-12-08 20:10:36 +0530

    batch-67

---
+++ batch-67/first.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# Author: kumar
+# usage: i want to greet the world hello.
+# log: 7th Dec - i created the program.
+#    : 8th dec - kumar - i added a smile to the program.
+print "hello world :) !!!"
