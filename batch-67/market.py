b09030837e62a539416c69032ec240e1d3e52612
tuxfux-hlp-notes <tuxfux.hlp@gmail.com>
2017-12-11 19:49:39 +0530

    variables and conditions

---
+++ batch-67/market.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# pass: a block filler
+# logical operators : and/or
+
+print "welcome to the market"
+food_type = raw_input("please enter your food type: veg/chicken/meat/fish - ")
+
+if food_type == 'chicken':
+	pass
+elif food_type == 'meat':
+	pass
+elif food_type == 'fish':
+	print "welcome to the fish market."
+	fish_type = raw_input("please enter yoru fish - solomon/tofu/rohu/fillets:")
+	if fish_type == 'rohu':
+		print "we have your {}".format(fish_type)
+		print "how much quantity of fish {} you need".format(fish_type)
+	elif fish_type == 'solomon':
+		print "we have your {}".format(fish_type)
+		print "how much quantity of fish {} you need".format(fish_type)
+	elif fish_type == 'tofu':
+		print "we have your {}".format(fish_type)
+		print "how much quantity of fish {} you need".format(fish_type)
+	else:
+		print "we DONT have your {}".format(fish_type)
+		print "how about chicken/meat/veg"
+elif food_type == 'veg':
+	pass
+else:
+	pass
