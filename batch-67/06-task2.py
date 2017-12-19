5c0ec8a703567a1d39cffa1fa1544ccf07ec8e22
tuxfux-hlp-notes <tuxfux.hlp@gmail.com>
2017-12-19 20:10:35 +0530

    dictionary

--- batch-67/06-task2.py
+++ batch-67/06-task2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
 my_fruits = ['apple','banana','apple','banana','cherry','apple']
 
@@ -7,12 +7,14 @@ my_dupli = ['apple','banana']
 my_fruits = ['apple','banana','cherry'] 
 
 '''
-my_fruits = ['apple','banana','apple','banana','cherry']
+my_fruits = ['apple','banana','apple','banana','cherry','apple']
+#my_fruits = ['apple','banana','apple','banana','cherry']
 my_dupli = []
-for value in my_fruits:
+for value in my_fruits[:]:
 	if my_fruits.count(value) > 1:
-		my_dupli.append(value)
+		if value not in my_dupli:
+			my_dupli.append(value)
 		my_fruits.remove(value)
 
-print my_dupli
-print my_fruits
+print (my_dupli)
+print (my_fruits)
