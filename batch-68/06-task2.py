d5d43f25ff6d55f5f80435d768d134b70c57fa1b
tuxfux-hlp-notes <tuxfux.hlp@gmail.com>
2018-02-06 19:59:06 +0530

    02-lists and tuples

--- batch-68/06-task2.py
+++ batch-68/06-task2.py
@@ -10,7 +10,18 @@ Output:
 my_fruits = ['apple','banana','cherry']
 my_dupli = ['apple','banana']
 
-
-
-
 '''
+
+my_dupli = []
+my_fruits = ['apple','banana','apple','banana','cherry','apple']
+print "my initial fruits {}".format(my_fruits)
+for value in my_fruits[:]:
+	print "my value is {}:".format(value)
+	if my_fruits.count(value) > 1:
+		if value not in my_dupli:
+			my_dupli.append(value)
+		my_fruits.remove(value)
+		#print my_fruits
+
+print my_fruits
+print my_dupli
