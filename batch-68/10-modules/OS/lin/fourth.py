f66af40c80b06cb811bb03d032aad9428efb867d
tuxfux-hlp-notes <tuxfux.hlp@gmail.com>
2018-02-12 19:57:54 +0530

    day-02Learning modules

---
+++ batch-68/10-modules/OS/lin/fourth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def my_first_lin4():
+	return "this is my first lin4 function."
+
+def my_second_lin4():
+	return "this is my second lin4 function."
+
+def my_third_lin4():
+	return "this is my third lin4 function."
+
+def my_fouth_lin4():
+	return "this is my fourth lin4 function."
---
+++ batch-68/10-modules/OS/sol/second.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def my_first_sol2():
+	return "this is my first sol2 function."
+
+def my_second_sol2():
+	return "this is my second sol2 function."
+
+def my_third_sol2():
+	return "this is my third sol2 function."
+
+def my_fouth_sol2():
+	return "this is my fourth sol2 function."
